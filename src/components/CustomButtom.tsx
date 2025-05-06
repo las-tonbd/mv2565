ecdc671c8a474067e603723856dd1c67d2a30de6
las-tonbd <tonbd@lasbom.com>
2025-05-06 09:13:05 +0700

    component button

--- src/components/CustomButtom.tsx
+++ src/components/CustomButtom.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import {Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
-export default function GradientButton() {
+type ButtonProps = {
+  title: string;
+  icon: any;
+  isActive: boolean;
+  onPress?: () => void;
+};
+
+export default function GradientButton({
+  title,
+  icon,
+  isActive,
+  onPress,
+}: ButtonProps) {
   return (
     <LinearGradient
       colors={['#09FBD3', '#130B2B']} // Viền gradient
       style={styles.gradientBorder}>
-      <TouchableOpacity activeOpacity={0.8} style={styles.innerContainer}>
-        <Image
-          source={require('../assets/icons/chart.png')}
-          style={styles.icon}
-        />
-        <Text style={styles.text}>Popular</Text>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        style={isActive ? styles.innerContainer : styles.innerContainerDefault}
+        onPress={onPress}>
+        <Image source={{uri: icon}} style={styles.icon} />
+        <Text style={styles.text}>{title}</Text>
       </TouchableOpacity>
     </LinearGradient>
   );
@@ -22,6 +34,7 @@ const styles = StyleSheet.create({
   gradientBorder: {
     padding: 3, // Độ dày viền
     borderRadius: 50,
+    marginHorizontal: 5,
   },
   innerContainer: {
     flexDirection: 'row',
@@ -34,6 +47,21 @@ const styles = StyleSheet.create({
     // Bóng đổ phía dưới
     shadowColor: '#00A1FF',
     shadowOffset: {width: 0, height: 6},
+    shadowOpacity: 0.4,
+    shadowRadius: 8,
+    elevation: 10,
+  },
+  innerContainerDefault: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.3)', // màu nền tím đậm bên trong
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 50,
+
+    // Bóng đổ phía dưới
+    shadowColor: '#00A1FF',
+    shadowOffset: {width: 0, height: 3},
     shadowOpacity: 0.4,
     shadowRadius: 8,
     elevation: 10,
